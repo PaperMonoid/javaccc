c2f1f56807b71a30c19bcff40919bb00d8702874
PaperMonoid <daniel.santiago.aguila.torres@gmail.com>
2019-04-24 12:46:15 -0700

    add: CLI arguments

--- src/__main__.py
+++ src/__main__.py
@@ -1,20 +1,56 @@
 import argparse
+import csv
+import os.path
+import time
 from regex import Regex
 from graph import Graph
+
 
 parser = argparse.ArgumentParser(
     description="A set of tools for Java Compiler Compiler™ (JavaCC™) made in python."
 )
 
-parser.add_argument(
-    "regex", metavar="r", type=str, nargs="+", help="a regular expression to compile"
-)
+parser.add_argument("-i", "--input-file", type=str, help="an input csv file")
+
+parser.add_argument("-o", "--output-dir", type=str, help="an output directory")
+
+
+def save_automata(regex, name, output_dir):
+    automata = Regex(regex)
+    automata_dict = automata.get_dict()
+    graph = Graph(
+        automata_dict.get("nodes"), automata_dict.get("edges"), name, output_dir
+    )
+    graph.Save()
+
 
 if __name__ == "__main__":
     args = parser.parse_args()
-    regex = " ".join(args.regex)
-    automata = Regex(regex)
-    automata_dict = automata.get_dict()
-    print(automata_dict)
-    graph = Graph(automata_dict.get("nodes"), automata_dict.get("edges"), "my_test")
-    graph.Save()
+    input_file = args.input_file
+    output_dir = args.output_dir
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    lines = 0
+    print("starting process...")
+    start_time = time.time()
+    with open(input_file) as csv_file:
+        reader = csv.reader(csv_file, delimiter=",")
+        for _ in reader:
+            lines = lines + 1
+    print("found {0} lines".format(lines))
+    count = 0
+    with open(input_file) as csv_file:
+        reader = csv.reader(csv_file, delimiter=",")
+        for [name, regex] in reader:
+            save_automata(regex, name, output_dir)
+            count = count + 1
+            print("done {0}/{1}".format(count, lines))
+    elapsed_time = time.time() - start_time
+    print("process finished!")
+    print(
+        "elapsed time: {0}".format(
+            time.strftime(
+                "%H hours, %M minutes and %S seconds", time.gmtime(elapsed_time)
+            )
+        )
+    )
